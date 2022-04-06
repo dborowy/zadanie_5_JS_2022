b5383490beaa55492e455120d8c999b9d10d99a2
dborowy <dominikborowy@gmail.com>
2022-04-06 15:26:07 +0200

    elo2

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,8 +1,40 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+  
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDWzWom-MZxx_6tiNdHz67AR-ZLW7VWLkI",
+    authDomain: "zadanie-5-12395.firebaseapp.com",
+    projectId: "zadanie-5-12395",
+    storageBucket: "zadanie-5-12395.appspot.com",
+    messagingSenderId: "734926560610",
+    appId: "1:734926560610:web:0779b00ddcb48d23c3203b"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig)
+  const db = firebase.firestore();
+  // db.collection("users").add({
+  // first: "Ada",
+  // last: "Lovelace",
+  // born: 1815
+  // })
+  // .then((docRef) => {
+  //     console.log("Document written with ID: ", docRef.id);
+  // })
+  // .catch((error) => {
+  //     console.error("Error adding document: ", error);
+    // });
+
 function logSubmit(event) {
   event.preventDefault();
-  const email = document.getElementById('exampleInputEmail1')
-  const name = document.getElementById('exampleInputName1')
-  const surname = document.getElementById('exampleInputSurname1')
+  const email = document.getElementById('exampleInputEmail1').value
+  const name = document.getElementById('exampleInputName1').value
+  const surname = document.getElementById('exampleInputSurname1').value
   db.collection("users").add({
     email: email,
     name: name,
@@ -18,5 +50,4 @@ function logSubmit(event) {
 }
 
 const form = document.getElementById('form');
-const log = document.getElementById('log');
 form.addEventListener('submit', logSubmit);
